91d1f2104b0174b0f51dbc02ea239e301d4b60b2
hyunsik2000 <ryuhsik2000@naver.com>
2026-01-16 11:33:21 +0900

    #1 chore: restructure monorepo

--- .storybook/main.ts
+++
@@ -1,9 +0,0 @@
-import type { StorybookConfig } from "@storybook/react-vite";
-
-const config: StorybookConfig = {
-  framework: "@storybook/react-vite",
-  stories: ["../stories/**/*.stories.@(ts|tsx)"],
-  addons: [],
-};
-
-export default config;
